7395f78e73b519189ef97a444b85a5b93f3110c5
Duy Le <lehongduy.bk@gmail.com>
2022-06-02 20:50:06 +0700

    update code

--- app/src/main/java/com/example/lesson8/MainActivity.java
+++ app/src/main/java/com/example/lesson8/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 102 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
